b553437b60a36b261c9ed39e8d8fb9c34d302903
Shruti Jain <sjshruti78@gmail.com>
2026-03-06 15:35:41 +0530

    Folder deletion

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Shruti Jain</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ab!</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
